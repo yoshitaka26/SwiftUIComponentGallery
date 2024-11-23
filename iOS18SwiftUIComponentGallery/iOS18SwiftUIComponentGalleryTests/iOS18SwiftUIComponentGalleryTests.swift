d8ba551bf813191b08bc9165d9d920bcf29d3d52
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2024-11-23 11:06:17 +0900

    update

---
+++ iOS18SwiftUIComponentGallery/iOS18SwiftUIComponentGalleryTests/iOS18SwiftUIComponentGalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS18SwiftUIComponentGalleryTests.swift
+//  iOS18SwiftUIComponentGalleryTests
+//
+//  Created by Yoshitaka Tanaka on 2024/11/23.
+//
+
+import Testing
+@testable import iOS18SwiftUIComponentGallery
+
+struct iOS18SwiftUIComponentGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
